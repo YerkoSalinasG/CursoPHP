bbc11c7525965a3dfd407d76ba71f5f91c4446dc
Yerko Salinas <yerkoj.salinasg@gmail.com>
2021-02-17 06:50:02 -0300

    Shortcode de un echo

--- 01-progdesde0/02-imprimir.php
+++ 01-progdesde0/02-imprimir.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,5 +17,6 @@
 			."</ul>";
 		// Concatenar con .
 	?>
+	<?="Shortcode de un Echo"?>
 </body>
 </html>
