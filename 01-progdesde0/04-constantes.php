2088fe8931bbc15383756ad6edbbc1b385e6f690
Yerko Salinas <yerkoj.salinasg@gmail.com>
2021-02-17 11:34:01 -0300

    constantes Predefinidas

--- 01-progdesde0/04-constantes.php
+++ 01-progdesde0/04-constantes.php
@@ -11,7 +11,23 @@
 		// Constantes son similar a las variables pero el valor NO CAMBIA se declaran.... define('nombredelaconstante', 'valor de la constante')
 
         define('nombre','Yerko Salinas Gómez');
-        echo nombre;
+        echo nombre; // se muestran sin el signo de dinero
+
+        //Constantes predefinidas
+        echo "<br>";
+        echo PHP_OS; // SISTEMA OPERATIVO
+        echo "<br>";
+        echo PHP_VERSION; // VERSION DE PHP
+        echo "<br>";
+        echo PHP_EXTENSION_DIR; // DONDE ESTAN LAS EXTENSIONES DE PHP
+        echo "<br>";
+        echo __LINE__; // MUESTRA LA LINEA DONDE ESTA EL CODIGO
+        echo "<br>";
+        echo __FILE__; // RUTA COMPLETA DEL ARCHIVO
+        echo "<br>";
+        echo __FUNCTION__; // MUESTRA LA FUNCION
+        echo "<br>";
+        // HAY MUCHAS...
 
 		
 	?>
