10e108376c6bd8a23c6be5b8d02f2874ceea068d
Yerko Salinas <yerkoj.salinasg@gmail.com>
2021-02-17 11:25:50 -0300

    constantes

---
+++ 01-progdesde0/04-constantes.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Constantes</title>
+</head>
+<body>
+	<?php
+		// Constantes son similar a las variables pero el valor NO CAMBIA se declaran.... define('nombredelaconstante', 'valor de la constante')
+
+        define('nombre','Yerko Salinas Gómez');
+        echo nombre;
+
+		
+	?>
+</body>
+</html>
