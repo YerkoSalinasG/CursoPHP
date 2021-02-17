3cf49864f4937131993465d1db9f1b070cfcc9a2
Yerko Salinas <yerkoj.salinasg@gmail.com>
2021-02-17 06:29:47 -0300

    Hola Mundo

---
+++ 01-progdesde0/01-progdesde0.php
@@ -0,0 +1,20 @@
+<?php
+/* Primeros pasos con PHP*/
+/*
+- Es fácil
+- Amplia documentación
+- es el más usado
+- es el más soportado
+- es la base de la web
+- capacidad de expansión (Librerías, Frameworks, CMS)
+
+Alternativas a PHP: Python, Ruby, Java, NodeJS*/
+
+// Hola mundo en PHP
+echo'Hola mundo';
+// Hola mundo con HTML
+echo'<h1>Hola mundo con HTML desde PHP</h1>';
+
+
+
+?>
